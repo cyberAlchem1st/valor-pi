eb99ec19a5494f4492594fedd56e486ec2390f13
Richard Bruno <richard.ufabc@gmail.com>
2021-03-13 01:08:35 -0300

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,4 +1,5 @@
 import random
+import math  
 
 n = int(input('Quantidade de pontos: '))
 circle = 0
@@ -6,8 +7,9 @@ square = 0
 for i in range(n):
   r = random.random()
   g = random.random()
-  result = (r**2)+(g**2)
+  result = math.sqrt((r**2)+(g**2))
 
+  
   if (result <= 1):
     circle += 1
   square += 1
